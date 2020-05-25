a8a4ac7e0513be358af313cdff7d68f3add10ec4
Rassul Yunussov <yunussov@gmail.com>
2020-05-26 00:00:51 +0600

    added two versions of crawling. no difference

--- Program.cs
+++ Program.cs
@@ -4,45 +4,58 @@ using System.Threading.Tasks;
 using System.Linq;
 using System.Collections.Concurrent;
 using System.Text;
+using System.Diagnostics;
 
 namespace tt
 {
     public class Page {
         public string Url { get; set; }
         public string Content { get; set; }
+        public List<string> Links {get;set;} 
     }
     class Program
     {
         static ConcurrentDictionary<string, Page> dic =  new ConcurrentDictionary<string,Page>();
         async Task<Page> Download(string url)
         {
+            Random r = new Random();
             return new Page() {
                 Url = url,
-                Content = "Some Content"
+                Content = "Some Content",
+                Links = Enumerable.Range(1,10).Select(x => r.Next(1,10000).ToString()).ToList()
             };
         }
-        string [] GetUrls(Page p)
+        IEnumerable<string> GetUrls(Page p)
         {
-            Random r = new Random();
-            List<string> output = new List<string>(1000);
-            for(int i = 0;i<1000;i++)
-            {
-                output.Add(r.Next(1,1000000).ToString());
-            }
-            return output.ToArray();
+           return p.Links;
+        }
+        
+        public async Task<Dictionary<string, Page>> crawlUrlsVersionOne(IEnumerable<string> urls)
+        {
+           List<Task<Task>> pageTasks = new List<Task<Task>>();
+           foreach(var url in urls.Except(dic.Keys))
+           {
+               pageTasks.Add(Download(url).ContinueWith( async pt => {
+                    var p = await pt;
+                    dic.TryAdd(url,p);
+                    var subUrls = GetUrls(p);
+                    await crawlUrlsVersionOne(subUrls);
+               }));
+           }
+           await Task.WhenAll(await Task.WhenAll(pageTasks));
+           return new Dictionary<string,Page>();
         }
 
-        public async Task<Dictionary<string, Page>> crawlUrls(string [] urls)
+        public async Task<Dictionary<string, Page>> crawlUrlsVersionTwo(IEnumerable<string> urls)
         {
            List<Task<Task<Task<Dictionary<string,Page>>>>> pageTasks = new List<Task<Task<Task<Dictionary<string,Page>>>>>();
            foreach(var url in urls.Except(dic.Keys))
            {
                pageTasks.Add(Download(url).ContinueWith( async pt => {
                     var p = await pt;
-                    System.Console.WriteLine(url);
                     dic.TryAdd(url,p);
                     var subUrls = GetUrls(p);
-                    return crawlUrls(subUrls);
+                    return crawlUrlsVersionTwo(subUrls);
                }));
            }
            await Task.WhenAll(await Task.WhenAll(await Task.WhenAll(pageTasks)));
@@ -52,7 +65,16 @@ namespace tt
         {
            Program p = new Program();
            string [] urls = new string []{"1","2","3"};
-           var dd = await p.crawlUrls(urls);
+           Stopwatch sw = new Stopwatch();
+           sw.Start();
+           await p.crawlUrlsVersionOne(urls);
+           sw.Stop();
+           System.Console.WriteLine(sw.Elapsed.TotalSeconds);
+           dic.Clear();
+           sw.Restart();
+           await p.crawlUrlsVersionTwo(urls);
+           sw.Stop();
+           System.Console.WriteLine(sw.Elapsed.TotalSeconds);
            System.Console.WriteLine(dic.Count);
         }
     }
